f243c9dc970a4fc2c56d050014271258c9f285de
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-19 09:19:59 +0100

    Update gaussian_mixture_models.py

--- gaussian_mixture_models.py
+++ gaussian_mixture_models.py
@@ -1,3 +1,8 @@
 import numpy as np
 from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as plt
+# Generiamo dei dati di esempio
+np.random.seed(0)
+n_samples = 1000
+n_features = 2
+n_components = 3
