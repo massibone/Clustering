57dc0ca922ee668fb2294f832670c4604df23855
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-20 08:11:01 +0100

    Update gaussian_mixture_models.py

--- gaussian_mixture_models.py
+++ gaussian_mixture_models.py
@@ -7,3 +7,7 @@ np.random.seed(0)
 n_samples = 1000
 n_features = 2
 n_components = 3
+# Generiamo dei dati da tre distribuzioni gaussiane diverse
+mean1 = [0, 0]
+cov1 = [[1, 0.5], [0.5, 1]]
+data1 = np.random.multivariate_normal(mean1, cov1, n_samples // 3)
