df26c4b80744f3dc4a99d29368590a12a611e0c0
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-22 10:33:47 +0100

    Update gaussian_mixture_models.py

--- gaussian_mixture_models.py
+++ gaussian_mixture_models.py
@@ -16,3 +16,7 @@ data1 = np.random.multivariate_normal(mean1, cov1, n_samples // 3)
 mean2 = [5, 5]
 cov2 = [[2, 1], [1, 2]]
 data2 = np.random.multivariate_normal(mean2, cov2, n_samples // 3)
+
+mean3 = [10, 10]
+cov3 = [[3, 2], [2, 3]]
+data3 = np.random.multivariate_normal(mean3, cov3, n_samples // 3
