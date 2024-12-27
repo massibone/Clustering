d9a586bde11b97f136955e05d7ecd66778dbd548
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-27 08:17:19 +0100

    Update gaussian_mixture_models2.py

--- gaussian_mixture_models2.py
+++ gaussian_mixture_models2.py
@@ -1,1 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
+from sklearn.model_selection import GridSearchCV
+from typing import List, Tuple
+
 
