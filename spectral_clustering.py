671e7777642b5384e22e60e8b611d7522d489927
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-01-07 08:06:57 +0100

    Update spectral_clustering.py

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -11,3 +11,8 @@ X = np.random.rand(n_samples, n_features)
 # Creiamo due cluster non lineari
 X[:100, :] += 2
 X[100:, :] -= 2
+
+# Eseguiamo il clustering spettrale
+n_clusters = 2
+sc = SpectralClustering(n_clusters=n_clusters, affinity='nearest_neighbors')
+labels = sc.fit_predict(X)
