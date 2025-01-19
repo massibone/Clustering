c9542b7736bd608e3e9a430bf478452b142e8c7f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-01-19 09:50:28 +0100

    Update spectral_clustering2.py

--- spectral_clustering2.py
+++ spectral_clustering2.py
@@ -1,11 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple, Optional
-
 from sklearn.cluster import SpectralClustering
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score, calinski_harabasz_score
-
 
 def generate_nonlinear_dataset(
     n_samples: int = 200, 
