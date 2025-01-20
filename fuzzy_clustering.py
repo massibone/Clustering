b51a56ebf0aa1a5c7ed6f348eed5a2c453588133
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-01-20 08:10:44 +0100

    Update fuzzy_clustering.py

--- fuzzy_clustering.py
+++ fuzzy_clustering.py
@@ -2,3 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skfuzzy.cluster import FuzzyCMeans
 
+# Generiamo un dataset di esempio
+np.random.seed(0)
+n_samples = 200
+n_features = 2
+X = np.random.rand(n_samples, n_features)
