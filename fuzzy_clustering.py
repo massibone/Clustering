15dc8c24c8427c5931a936dbc9ab51043790a0b9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-01-22 08:06:33 +0100

    Update fuzzy_clustering.py

--- fuzzy_clustering.py
+++ fuzzy_clustering.py
@@ -12,6 +12,7 @@ X = np.random.rand(n_samples, n_features)
 # Creiamo due cluster non lineari
 X[:100, :] += 2
 X[100:, :] -= 2
+
 # Eseguiamo il clustering fuzzy
 n_clusters = 2
 fcm = FuzzyCMeans(n_clusters=n_clusters, m=2, error=0.005, maxiter=1000, init=None)
